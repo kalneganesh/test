33ee6ee87b8a2e82f441e5013bf5d13715a3ce27
GANESH KALNE <66562863+kalneganesh@users.noreply.github.com>
2023-02-03 17:24:17 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,11 @@
+sudo yum -y install python3-pip
+git clone https://github.com/Digvijay1133/Fuel-Consumption-python.git
+cd Fuel-Consumption-python
+pip3 install -r requirements.txt
+screen -m -d python3 app.py
+
+
+git clone https://github.com/Digvijay1133/agri-python.git
+cd agri-python
+pip3 install -r requirements.txt
+screen -m -d python3 app.py
